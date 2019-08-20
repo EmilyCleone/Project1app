32e091c6094b12a19a88fce6b6cb773224aad5a6
Emily Johnson-Eaton <emilyjohnsoneaton@gmail.com>
2019-08-19 20:29:52 -0600

    store user input in local storage

--- assets/app.js
+++ assets/app.js
@@ -19,6 +19,31 @@
 
 //====================Dallin End==========================//
 //====================Emily's Section=====================//
+$("#submit-button").on("click", function(event) {
+  event.preventDefault();
+
+  // Capture User Inputs and store them into variables
+  var name=$("#name-input").val();
+  var budget= $("#budget-input").val();
+  var time=$("#time-input").val();
+  var age=$("#age-input").val();
+  var location=$("#location-input").val();
+
+  // Console log each of the user inputs to confirm we are receiving them correctly
+  console.log(name);
+  console.log(budget);
+  console.log(time);
+  console.log(age);
+  console.log(location);
+
+
+  localStorage.setItem("name-input", name);
+  localStorage.setItem("budget-input", budget);
+  localStorage.setItem("time-input", time);
+  localStorage.setItem("age-input", age);
+  localStorage.setItem("location-input", location);
+});
+
 
 
 $("#submit-button").on("click", function (event) {
